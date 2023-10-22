24e9814a80249e1fc0b5b6b8583b5ddfd596662a
Trenton Creamer <trentoncreamer@yahoo.com>
2023-10-22 09:17:06 -0700

    Finished lab with full coverage

--- Lab6/todo_app/src/main.py
+++ Lab6/todo_app/src/main.py
@@ -7,37 +7,32 @@ app = FastAPI()
 
 @app.get("/api")
 # Get api 
-def first_api():
+def get_api():
     return {"msg" : "hello_world"}
 
 # Create a GET ReST API with path parameters
 # Need to use %20 as spaces
 @app.get("/books/{path_param}")
-def first_apiV2(path_param: str):
+def get_apiV2(path_param: str):
     return({"msg": path_param})
 
 # Create a GET ReST API with query parameters
 # fast api knows that it is a query parameter because of the missing {} in the url 
 @app.get("/books/")
-def first_apiV3(title: str):
+def get_apiV3(title: str):
     return {"msg": title}
 
 # Create a GET ReST API with path parameters AND query parameters
 @app.get("/books/{path_param}/")
-def first_apiV4(path_param: str, actor: str):
+def get_apiV4(path_param: str, actor: str):
     # Curl command to test is 
     # curl 'http://127.0.0.1:8000/books/main_actor/?actor=Harry'
     return {"msg": path_param, "actor": actor}
 
-# Get request to get all of the books
-@app.get("/books/")
-def first_apiV5():
-    return {"msg": "all books"}
-
 # Create a POST ReST API
 #  curl 'http://127.0.0.1:8000/books/create_book' -d "{'title': 'Harry Potter', 'author' : 'J.K Rowling', 'category': 'fiction'}" 
 @app.post("/books/create_book")
-def first_api_post(new_book = Body()):
+def post_api(new_book = Body()):
     print(new_book)
     return {"msg": new_book}
 
@@ -45,13 +40,14 @@ def first_api_post(new_book = Body()):
 # Create a PUT ReST API 
 # Put is for updating existing data
 @app.put("/books/update_book")
-def first_api_put(update_book = Body()):
+def put_api(update_book = Body()):
     print(f"The book has been updated {update_book}")
     return {"msg": update_book}
 
 
 # Create a DELETE ReST API
 @app.delete("/books/delete_book")
-def first_api_delete(delete_book = Body()):
+def delete_api(title:str, author:str, category:str):
+    delete_book = {"title": title, "author": author, "category": category}
     print(f"The book has been deleted {delete_book}")
     return {"msg": delete_book}
--- Lab6/todo_app/tests/test_api.py
+++ Lab6/todo_app/tests/test_api.py
@@ -7,13 +7,61 @@ from fastapi.testclient import TestClient
 
 from src.main import app
 
-#python -m pytest tests/
+# python3 -m pytest tests/
+# python3 -m pytest --cov=src --cov-report=html tests/
+
 
 # Create an instance of the TestClient class
 client = TestClient(app)
 
 def test_get_api():
-    print("Test")
     response = client.get("/api")
     assert response.status_code == 200
     assert response.json() == {"msg": "hello_world"}
+
+# Testing the get request with path parameters
+def test_get_apiV2():
+    response = client.get("/books/Harry%20Potter")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Harry Potter"}
+
+# Testing the request with query parameters
+def test_get_apiV3():
+    response = client.get("/books/?title=Harry%20Potter")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Harry Potter"}
+
+def test_get_apiV4():
+    response = client.get("/books/main_actor/?actor=Harry")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "main_actor", "actor": "Harry"}
+
+def test_post_api():
+    json_payload = {
+        "title": "Harry Potter",
+        "author": "J.K Rowling",
+        "category": "fiction"
+    }
+    # json=json_payload is the body of the request
+    response = client.post("/books/create_book", json=json_payload)
+    assert response.status_code == 200
+    assert response.json() == {"msg": json_payload}
+
+def test_put_api():
+    json_payload = {
+        "title": "Trenton Potter",
+        "author": "J.K Creamer",
+        "category": "non-fiction"
+    }
+    response = client.put("/books/update_book",json = json_payload)
+    assert response.status_code == 200
+    assert response.json() == {"msg": json_payload}
+
+def test_delete_api():
+    book_to_delete = {
+        "title": "Trenton Potter",
+        "author": "J.K Creamer",
+        "category": "non-fiction"
+    }
+    response = client.delete("/books/delete_book",params=book_to_delete)
+    assert response.status_code == 200
